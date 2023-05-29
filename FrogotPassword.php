0fb1a99ac088598342fbd0b001aa569616dc64a2
@Sanoj.de.silva## <91735772+Sanojdesilva@users.noreply.github.com>
2023-05-29 20:56:03 +0530

    Add files via upload

---
+++ FrogotPassword.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <link rel="stylesheet" href="style.css">
+<title>Frogot Password Window</title>
+</head>
+	
+<body>
+    <div style="background:linear-gradient(to top, rgba(0, 0, 0, 0.5)50%, rgba(0, 0, 0, 0.5)50%), url(Images/note1.jpg);height:100vh; background-repeat: no-repeat;background-size: cover;">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">NeatNote</h2>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li> <a href="#">Home</a></li>
+                    <li> <a href="#">About</a></li>
+                    <li> <a href="#">Contact</a></li>
+                    <li> <a href="#">Help</a></li>
+                    <li> <a href="#">SignUp</a></li>
+                </ul>
+            </div>
+        </div>
+        <p>&nbsp;	</p>
+        <div>
+        <form id="form1" name="form1" method="post" action="#">
+            <table width="887" height="462" cellspacing="0" cellpadding="0" class="tbl01" style="margin-top:auto; margin-bottom: auto; margin-left: auto;margin-right: auto;">
+            <caption style="width: 220px;
+            font-family: sans-serif;
+            text-align: center;
+            color: #00ff48;
+            font-size: 22px;
+            background-color: rgb(0, 0, 0);
+            border-radius: 10px;
+            padding-top: 10px;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: auto;
+            margin-bottom: auto;
+            padding: 8px">
+                Frogot Password
+            </caption>
+            <tbody>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                <td>Enter New Password  : </td>
+                <th scope="col"><input type="password" name="NewPassword" id="npwd" placeholder="Enter your New Password"></th>
+                </tr>
+                <tr>
+                <td>Confirm New Password:</td>
+                <th scope="col"><input type="password" name="Confirmpwd" id="cpwd" placeholder="Confirm Your Password"></th>
+                </tr>
+                <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                </tr>
+                <tr>
+                <td>s </td>
+                <th scope="col"><button class="submit1" name="Confirm" id="Confirm">Confirm</button></th>
+                </tr>
+            </tbody>
+            </table>
+        </form>
+        </div>
+    <div>
+</body>
+</html>
